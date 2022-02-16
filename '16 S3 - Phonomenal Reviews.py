a20150b82342055bd1a7f59a5d44fe61ae816fc3
PizzaPrix <seanwaterloo@rogers.com>
2022-02-16 10:41:29 -0500

    Completed the '17 S2

--- '16 S3 - Phonomenal Reviews.py
+++ '16 S3 - Phonomenal Reviews.py
@@ -1,7 +1,18 @@
 # https://dmoj.ca/problem/ccc16s3
 
+
+# Data Collecction
 userInput = input().split(" ")
 
-N = userInput[0]
-M = userInput[1]
+N = int(userInput[0])
+M = int(userInput[1])
+Pho = []
+paths = []
+
+for i in input().split(" "):
+    Pho.append(int(i))
+
+# Receive the N - 1 paths
+for i in range(N - 1):
+    paths.append(input())
 
